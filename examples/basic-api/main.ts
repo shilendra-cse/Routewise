19335355114ec6e375070c9e825c912055e37393
Shilendra Singh <shelendrasingh704@gmail.com>
2026-06-17 22:45:18 +0530

    added build package in example/basic-api

--- examples/basic-api/main.ts
+++ examples/basic-api/main.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { routewise } from "../../src/index.js";
+import { routewise } from "routewise";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const resourcesDir = path.join(__dirname, "resources");
--- examples/basic-api/resources/users/[id]/route.get.ts
+++ examples/basic-api/resources/users/[id]/route.get.ts
@@ -1,4 +1,4 @@
-import type { Context } from "../../../../../src/shared/types.js";
+import type { Context } from "routewise";
 
 export function handler(ctx: Context) {
   return ctx.json({ id: ctx.params.id });
